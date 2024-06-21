06f40e0c202c35f42d25cbf948e20af3719a0577
Marcel Stadelmann <m@marceli.to>
2024-06-21 11:13:10 +0200

    wip

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -1,7 +1,14 @@
 <x-app-layout>
   <div class="max-width-page pt-24 font-medium">
-    <p>Vorprojekt</p>
-    <p>Bauprojekt</p>
-    <p>Ausschreibung</p>
+    <p>Baubeginn Hochbau<br>Mitte Februar 2024</p>
+    <p>Decke über 1. UG<br>Ende Juni 2024</p>
+    <p>Beginn Haustechnik<br>Anfangs April 2025</p>
+    <p>Rohbauvollendung, Aufrichte<br>Mitte Juli 2025</p>
+    <p>Umgebung Pocket Park<br>Anfangs September 2025</p>
+    <p>Umgebung Allgemein<br>Anfangs März 2026</p>
+    <p>Fertigstellung Fassade, Flachdach<br>Mitte Oktober 2025</p>
+    <p>Beginn Ausbau<br>Anfangs November 2025</p>
+    <p>Fertigstellung Solarfassade Süd<br>Ende Dezember 2025</p>
+    <p>Bauübergabe<br>Ende November 2026</p>
   </div>
 </x-app-layout>
--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout>
   <div class="max-width-page pt-24 font-medium">
-    <p>Bauherrschaft<br>Keller Druckmesstechnik AG, Winterthur</p>
+    <p>Bauherrschaft<br>KELLER Services & Immobilien AG, Winterthur</p>
     <p>Generalplaner<br>GP Generalplaner AG, Zürich</p>
     <p>Architekt<br>Strut Architekten AG, Winterthur</p>
     <p>Bauleitung<br>MMT AG Bauleiter und Architekten, Zürich</p>
+    <p>Bauingenieur<br>S+K Bauingenieure AG, Winterthur</p>
+    <p>Elektroingenieur<br>enerpeak ag, Winterthur</p>
+    <p>HLKS-Ingenieur<br>Beag Engineering AG, Winterthur</p>
+    <p>Bauherrenberater<br>MMK Immobilientreuhand AG, Zürich</p>
+    <p>Bauherrenberater<br>BBS Ingenieur AG, Winterthur</p>
   </div>
 </x-app-layout>
