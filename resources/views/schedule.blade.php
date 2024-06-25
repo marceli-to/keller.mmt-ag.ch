0c94dfad2354eb59dc2d5951dbd39039d193de1e
Marcel Stadelmann <m@marceli.to>
2024-06-25 16:52:59 +0200

    wip

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -1,13 +1,12 @@
 <x-app-layout>
   <div class="max-width-page pt-24 font-medium">
-    <p>Baubeginn Hochbau<br>Mitte Februar 2024</p>
-    <p>Decke über 1. UG<br>Ende Juni 2024</p>
-    <p>Beginn Haustechnik<br>Anfangs April 2025</p>
-    <p>Rohbauvollendung, Aufrichte<br>Mitte Juli 2025</p>
+    <p>Baubeginn Hochbau<br>Anfangs März 2024</p>
+    <p>Beginn Haustechnik<br>Anfangs Dezember 2024</p>
+    <p>Rohbauvollendung, Aufrichte<br>Ende März 2025</p>
     <p>Umgebung Pocket Park<br>Anfangs September 2025</p>
     <p>Umgebung Allgemein<br>Anfangs März 2026</p>
     <p>Fertigstellung Fassade, Flachdach<br>Mitte Oktober 2025</p>
-    <p>Beginn Ausbau<br>Anfangs November 2025</p>
+    <p>Beginn Ausbau<br>Anfangs August 2025</p>
     <p>Fertigstellung Solarfassade Süd<br>Ende Dezember 2025</p>
     <p>Bauübergabe<br>Ende November 2026</p>
   </div>
